db97cad2bd909a01ae3a1450ab41f3e0bae96cdd
KothaSathish <sathish7chary@gmail.com>
2017-11-17 00:00:59 +0530

    parent events binding added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,31 @@ export class AppComponent implements OnInit {
   isServiceEx = false;
   isAppParent = false;
   isReader = false;
+  isAppParentChild = false;
    constructor() { }
    appParent(event){
 	 this.isServiceEx = false;
 	 this.isAppParent = true;
 	 this.isReader = false;
+	 this.isAppParentChild = false;
    }
    appServiceEx(event){
 	   this.isServiceEx = true;
 	   this.isAppParent = false;
 	   this.isReader = false;
+	   this.isAppParentChild = false;
    }
    appReader(event){
 	   this.isServiceEx = false;
        this.isAppParent = false;
 	   this.isReader = true;
+	   this.isAppParentChild = false;
+   }
+   appParentchild(){
+	   this.isServiceEx = false;
+       this.isAppParent = false;
+       this.isReader = false;
+       this.isAppParentChild = true; 
    }
 
   ngOnInit() {
